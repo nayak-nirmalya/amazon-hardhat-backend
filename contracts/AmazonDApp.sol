a53a82706260a6b1e8cc71dda0263e456d870cff
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-18 12:40:19 +0530

    added types to contract and factory

--- contracts/AmazonDApp.sol
+++ contracts/AmazonDApp.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract AmazonDApp {
-    string public name = "AmazonDApp";
+    string public name;
 
     // console.log("Contract Name: %o", name);
+    constructor() {
+        name = "AmazonDApp";
+    }
 }
--- test/AmazonDApp.ts
+++ test/AmazonDApp.ts
@@ -2,14 +2,25 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { AmazonDApp, AmazonDApp__factory } from "../typechain-types";
+
+const tokens = (no: number) => ethers.utils.parseUnits(no.toString(), "ether");
 
 describe("AmazonDApp", () => {
+  let amazonDApp: AmazonDApp;
+
+  beforeEach(async () => {
+    // deploy contract
+    const AmazonDApp = (await ethers.getContractFactory(
+      "AmazonDApp"
+    )) as AmazonDApp__factory;
+    amazonDApp = await AmazonDApp.deploy();
+  });
+
   describe("Smoke Test", () => {
     it("Should Print Contract Name", async () => {
-      const AmazonDApp = await ethers.getContractFactory("AmazonDApp");
-      const amazonDApp = await AmazonDApp.deploy();
-
-      expect(await amazonDApp.name()).to.equal("AmazonDApp");
+      const name = await amazonDApp.name();
+      expect(name).to.equal("AmazonDApp");
     });
   });
 });
