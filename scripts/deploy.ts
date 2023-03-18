a4b774cfc9378d491833a76775a772bb104ee0b3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-18 18:50:15 +0530

    added deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,8 @@
 import { ethers } from "hardhat";
-import items from "../items.json";
+import { items } from "../items.json";
 import { AmazonDApp, AmazonDApp__factory } from "../typechain-types";
+
+const tokens = (no: string) => ethers.utils.parseUnits(no.toString(), "ether");
 
 async function main() {
   // get signers
@@ -14,7 +16,26 @@ async function main() {
 
   await amazonDApp.deployed();
 
-  console.log(`AmazonDApp deployed to ${amazonDApp.address}`);
+  console.log(`AmazonDApp Deployed to ${amazonDApp.address}`);
+
+  // list items
+  items.map(async (item) => {
+    const transaction = await amazonDApp
+      .connect(owner)
+      .list(
+        item.id,
+        item.name,
+        item.category,
+        item.image,
+        tokens(item.price),
+        item.rating,
+        item.stock
+      );
+
+    await transaction.wait();
+
+    console.log(`Listed Item ${item.id}: ${item.name}`);
+  });
 }
 
 main().catch((error) => {
